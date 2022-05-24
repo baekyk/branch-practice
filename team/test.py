08317034a5ae56fc5c3ec0e31ccf5d138464f0c9
baekyk <baekyk23@naver.com>
2022-05-24 21:53:26 +0900

    feat: ipynb test

--- team/test.py
+++ team/test.py
@@ -3,3 +3,10 @@ print('hello')
 for i in range(10,20):
     print(i, end=' ')    
 print('\nbye')
+
+nums= [1,7,3,6,5,6]
+sums=[]
+sums.append(nums[0])
+for i in range(len(nums)):
+    sums.append(sums[i-1]+nums[i])
+print(sums)
